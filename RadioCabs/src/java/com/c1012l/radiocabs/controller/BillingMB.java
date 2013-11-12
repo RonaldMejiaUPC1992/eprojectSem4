f263c7d68082fad5b69ad2c5120f4ce673ca28ab
duonghm56 <duonghm_56@vnu.edu.vn>
2013-11-12 16:50:25 +0700

    add controller package, model package

---
+++ RadioCabs/src/java/com/c1012l/radiocabs/controller/BillingMB.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.c1012l.radiocabs.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author DUONGHM
+ */
+@ManagedBean
+@RequestScoped
+public class BillingMB {
+
+    /**
+     * Creates a new instance of BillingMB
+     */
+    public BillingMB() {
+    }
+}
---
+++ RadioCabs/src/java/com/c1012l/radiocabs/model/BillingModel.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.c1012l.radiocabs.model;
+
+/**
+ *
+ * @author DUONGHM
+ */
+public class BillingModel {
+    
+}
