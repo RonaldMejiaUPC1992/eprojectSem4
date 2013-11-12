c6f712fa1e41bbdad2b3dcf21e60857bd8e37119
duonghm56 <duonghm_56@vnu.edu.vn>
2013-11-12 16:42:06 +0700

    fix database

--- radioCabs.sql
+++ radioCabs.sql
@@ -5,7 +5,7 @@ go
 
 create table paymentType(
 	paymentTypeID int primary key,
-	paymentTypeName nvarchar(100)
+	paymentTypeName varchar(100)
 )
 go
 insert into paymentType values (1, 'Monthly'), (2, 'Quartly')
@@ -13,31 +13,31 @@ go
 
 create table registeredType(
 	registeredTypeID int primary key,
-	registeredTypeName nvarchar(200) not null
+	registeredTypeName varchar(200) not null
 )
 go
 insert into registeredType values (1, 'Company'), (2, 'Driver'), (3, 'Advertise')
 go
 create table registeredUnit(
 	registrationID varchar(10) primary key,
-	name nvarchar(200) not null,	
-	password varchar(50) not null,
-	contactPerson nvarchar(200),
-	designation nvarchar(100),
+	name varchar(200) not null,	
+	password varchar(50),
+	contactPerson varchar(200),
+	designation varchar(100),
 	faxNumber varchar(20),
 	membershipType int,		
-	city nvarchar(100),
+	city varchar(100),
 	experience int,
-	description nvarchar(200),		
-	address nvarchar(200),
+	description varchar(200),		
+	address varchar(200),
 	mobile varchar(20),
 	telephone varchar(20),
 	email varchar(100),
 	registeredTypeID int references registeredType(registeredTypeID),		
 )
 go
-create table payment(
-	paymentID int identity primary key,
+create table billing(
+	billingID int identity primary key,
 	registrationID varchar(10) references registeredUnit(registrationID) not null,
 	paidTypeID int references paymentType(paymentTypeID) not null,
 	money float not null,
@@ -48,7 +48,7 @@ go
 
 create table feedbackType(
 	feedbackTypeID int primary key,
-	feedbackTypeName nvarchar(100) not null
+	feedbackTypeName varchar(100) not null
 )
 go
 insert into feedbackType values(1, 'Complaint'), (2, 'Suggestion'), (3, 'Compliment')
@@ -56,11 +56,11 @@ go
 create table feedback(
 	feedbackID int identity primary key,
 	feedbackTypeID int references feedbackType(feedbackTypeID) not null,
-	name nvarchar(200) not null,
+	name varchar(200) not null,
 	moblie varchar(20),
 	email varchar(100),
-	city nvarchar(100),
-	description nvarchar(200)
+	city varchar(100),
+	description varchar(200)
 )
 go
 
