9d129106579fa6dabdedebbd5f4950dd28146c6d
duonghm56 <duonghm_56@vnu.edu.vn>
2013-11-17 15:10:23 +0700

    feedback Form

--- RadioCabs/src/java/org/group2/controller/ControllerFeedback.java
+++ RadioCabs/src/java/org/group2/controller/ControllerFeedback.java
@@ -4,11 +4,15 @@
  */
 package org.group2.controller;
 
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
 import org.group2.entity.Feedback;
+import org.group2.entity.FeedbackType;
 import org.group2.entity.RegisteredUnit;
+import org.group2.model.AbstractModel;
 
 /**
  *
@@ -18,8 +22,24 @@ import org.group2.entity.RegisteredUnit;
 @ViewScoped
 public class ControllerFeedback extends AbstractController {
     
+    private List<FeedbackType> listFeedbackType;
+    
     public ControllerFeedback() {
-        super(Feedback.class);
+        super(Feedback.class);         
+        listFeedbackType = new AbstractModel<FeedbackType>(FeedbackType.class){}.getAll();               
+    }
+
+    public List<FeedbackType> getListFeedbackType() {
+        selected = new Feedback();
+        return listFeedbackType;
+    }
+
+    public void setListFeedbackType(List<FeedbackType> listFeedbackType) {
+        this.listFeedbackType = listFeedbackType;
+    }
+    
+    public void sendFeedfack(ActionEvent evt){
+        System.out.println("====Call send feedback ???======");
     }
     
 }
