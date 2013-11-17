5faff00b1fb929bacce001322470ba0e626be303
duonghm56 <duonghm_56@vnu.edu.vn>
2013-11-17 23:40:12 +0700

    fix login and display Billing (fixed)

--- RadioCabs/src/java/org/group2/controller/ControllerBilling.java
+++ RadioCabs/src/java/org/group2/controller/ControllerBilling.java
@@ -4,10 +4,14 @@
  */
 package org.group2.controller;
 
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
 import org.group2.entity.Billing;
+import org.group2.entity.RegisteredUnit;
 
 /**
  *
@@ -22,5 +26,14 @@ public class ControllerBilling extends AbstractController {
      */
     public ControllerBilling() {
         super(Billing.class);
+    }
+    
+    public List<Billing> getCurrentUserBillings(){
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpSession session =
+                (HttpSession) context.getExternalContext().getSession(true);        
+        RegisteredUnit currentUser = (RegisteredUnit)session.getAttribute("clientUser");
+        String registrationID = "'" + currentUser.getRegistrationId() + "'";        
+        return search("registrationId = "+registrationID);
     }
 }
