44d334b3d8146553d17c601a59dee993adfa14bd
duonghm56 <duonghm_56@vnu.edu.vn>
2013-11-24 21:35:17 +0700

    more update

--- RadioCabs/src/java/org/group2/model/AbstractModel.java
+++ RadioCabs/src/java/org/group2/model/AbstractModel.java
@@ -15,89 +15,106 @@ import org.hibernate.Transaction;
  * @author DUONGHM
  */
 public abstract class AbstractModel<T> {
-    
+
     private Class<T> entityClass;
-    
-    public AbstractModel(){        
+
+    public AbstractModel() {
     }
-    
+
     public AbstractModel(Class<T> entityClass) {
         this.entityClass = entityClass;
     }
-    
-    public List<T> getAll(){        
+
+    public List<T> getAll() {
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction trans = session.beginTransaction();
         trans.begin();
-        Query query = session.createQuery("FROM " + entityClass.getName());    
+        Query query = session.createQuery("FROM " + entityClass.getName());
         //System.out.println("================FROM " + entityClass.getName() + "=======================");
         List<T> lst = query.list();
         trans.commit();
         session.close();
         return lst;
     }
-    
-    public List<T> getAll(String condition){
+
+    public List<T> getAll(String condition) {
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction trans = session.beginTransaction();
         trans.begin();
-        Query query = session.createQuery("FROM " + entityClass.getName()+ " WHERE "+condition);    
+        Query query = session.createQuery("FROM " + entityClass.getName() + " WHERE " + condition);
         //System.out.println("======FROM " + entityClass.getName()+ " WHERE "+condition+"======");
         List<T> lst = query.list();
         trans.commit();
         session.close();
         return lst;
     }
-    
-    public List<T> createHQLQuery(String hqlQuery){
+
+    public List<T> createHQLQuery(String hqlQuery) {
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction trans = session.beginTransaction();
         trans.begin();
-        Query query = session.createQuery(hqlQuery);    
+        Query query = session.createQuery(hqlQuery);
         //System.out.println("======FROM " + entityClass.getName()+ " WHERE "+condition+"======");
         List<T> lst = query.list();
         trans.commit();
         session.close();
         return lst;
     }
-    
-    public List<T> search(String condition){
+
+    public List<T> search(String condition) {
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction trans = session.beginTransaction();
         trans.begin();
-        Query query = session.createQuery("FROM " + entityClass.getName()+ " WHERE "+condition);    
+        Query query = session.createQuery("FROM " + entityClass.getName() + " WHERE " + condition);
         //System.out.println("======FROM " + entityClass.getName()+ " WHERE "+condition+"======");
         List<T> lst = query.list();
         trans.commit();
         session.close();
         return lst;
     }
-    
-    public void add(T entity){
+
+    public void add(T entity) throws Exception{
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction trans = session.beginTransaction();
-        trans.begin();
-        session.save(entity);
-        trans.commit();
+        try {            
+            trans.begin();
+            session.save(entity);
+            trans.commit();            
+        } catch (Exception ex) {
+            trans.rollback();            
+            ex.printStackTrace();            
+            throw new Exception(ex.getMessage());
+        }        
         session.close();
     }
-    
-    public void update(T entity){
+
+    public void update(T entity) throws Exception{
         Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction trans = session.beginTransaction();
-        trans.begin();
-        session.update(entity);
-        trans.commit();
+        Transaction trans = session.beginTransaction();        
+        try {
+            trans.begin();
+            session.update(entity);
+            trans.commit();
+        } catch (Exception ex) {
+            trans.rollback();
+            ex.printStackTrace();  
+            throw new Exception(ex.getMessage());
+        }        
         session.close();
     }
-    
-    public void delete(T entity){
+
+    public void delete(T entity) throws Exception {
         Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction trans = session.beginTransaction();
-        trans.begin();
-        session.delete(entity);
-        trans.commit();
+        Transaction trans = session.beginTransaction();        
+        try {
+            trans.begin();
+            session.delete(entity);
+            trans.commit();
+        } catch (Exception ex) {
+            trans.rollback();
+            ex.printStackTrace();  
+            throw new Exception(ex.getMessage());
+        }        
         session.close();
     }
-    
 }
