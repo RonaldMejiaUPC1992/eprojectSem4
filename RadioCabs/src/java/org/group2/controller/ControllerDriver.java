6c8aa598eb846f329ca5a33f36a27e49e698e088
duonghm56 <duonghm_56@vnu.edu.vn>
2013-11-27 02:29:42 +0700

    upload file success

--- RadioCabs/src/java/org/group2/controller/ControllerDriver.java
+++ RadioCabs/src/java/org/group2/controller/ControllerDriver.java
@@ -5,19 +5,23 @@
 package org.group2.controller;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
-import org.apache.tomcat.util.http.fileupload.FileUtils;
-import org.group2.entity.Billing;
+import javax.servlet.ServletContext;
 import org.group2.entity.RegisteredType;
 import org.group2.entity.RegisteredUnit;
+import org.primefaces.component.fileupload.FileUpload;
 import org.primefaces.event.FileUploadEvent;
-import org.primefaces.event.RowEditEvent;
+import org.primefaces.model.UploadedFile;
 
 /**
  *
@@ -32,7 +36,7 @@ public class ControllerDriver extends AbstractController implements Serializable
     }
 
     @Override
-    public List getList() {        
+    public List getList() {
         return super.getList("registeredTypeID = 2");
     }
 
@@ -43,15 +47,13 @@ public class ControllerDriver extends AbstractController implements Serializable
     }
 
     @Override
-    public void update(ActionEvent evt) {          
+    public void update(ActionEvent evt) {
         super.update(evt);
     }
-    
-    
 
     public List getDisplayList() {
         //return super.createHQLQuery("select r From RegisteredUnit r join r.billings b where r.registeredType.registeredTypeId = 2 and b.expriateDate > CURRENT_DATE() " + condition);
-        String hqlQuery = "select r From RegisteredUnit r join r.billings b where r.registeredType.registeredTypeId = 2 and b.expriateDate > CURRENT_DATE() " + condition;                
+        String hqlQuery = "select r From RegisteredUnit r join r.billings b where r.registeredType.registeredTypeId = 2 and b.expriateDate > CURRENT_DATE() " + condition;
         return super.createHQLQuery(hqlQuery);
     }
     RegisteredUnit searchedUnit = new RegisteredUnit();
@@ -66,15 +68,44 @@ public class ControllerDriver extends AbstractController implements Serializable
     }
 
     public void searchByName(ActionEvent evt) {
-        selected = new RegisteredUnit();        
+        selected = new RegisteredUnit();
         condition = " and r.name like '%" + searchedUnit.getName() + "%'";
-        condition += " and r.city like '%" + searchedUnit.getCity()+ "%'";
+        condition += " and r.city like '%" + searchedUnit.getCity() + "%'";
         condition += " and r.telephone like '%" + searchedUnit.getTelephone() + "%'";
         condition += " and r.experience >= " + searchedUnit.getExperience();
     }
+
+    public void handleImageUpload(FileUploadEvent evt) {
+        try {
+            String path = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/");                 
+            File targetFile = new File(path+"Images/Drivers/"+evt.getFile().getFileName());            
+            InputStream inputStream = evt.getFile().getInputstream();
+            OutputStream out = new FileOutputStream(targetFile);
+            int read = 0;
+            byte[] bytes = new byte[1024];
+            while ((read = inputStream.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+            inputStream.close();
+            out.flush();
+            out.close();
+            System.out.println(targetFile.getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     
-    public void handleImageUpload(FileUploadEvent evt){
-        //if((RegisteredUnit))
-        //File dest = new File(condition)
+    UploadedFile uploadFile;
+
+    public UploadedFile getUploadFile() {
+        return uploadFile;
+    }
+
+    public void setUploadFile(UploadedFile uploadFile) {
+        this.uploadFile = uploadFile;
+    }
+    
+    public void upload(){
+        System.out.println(uploadFile.getFileName());
     }
 }
